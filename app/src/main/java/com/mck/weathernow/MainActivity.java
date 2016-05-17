172495a64236a6a57608124404468dcd05983c40
Michael King <michaeldigit@gmail.com>
2016-05-17 14:18:48 -0700

    include google play services
    include code to get a googleApi Connection then location. include code to check for internet connection and permission changes.

--- app/src/main/java/com/mck/weathernow/MainActivity.java
+++ app/src/main/java/com/mck/weathernow/MainActivity.java
@@ -1,39 +1,74 @@
 package com.mck.weathernow;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
-    private static final int MY_PERMISSIONS_REQUEST = 72;
-    private boolean needsDeniedPermission = false;
+    private static final int MY_PERMISSIONS_REQUEST = 72; // for when making permissions
+    private boolean needsDeniedPermission = false; // if was denied a necessary permission.
+    private CurrentWeatherFragment currentWeatherFragment;
+    private ForecastWeatherFragment forecastWeatherFragment;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!hasRequiredPermissions()){
-            requestRequiredPermissions();
+        // if there is no saved instance state
+        if (savedInstanceState == null) {
+            currentWeatherFragment = new CurrentWeatherFragment();
+            forecastWeatherFragment = new ForecastWeatherFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.current_weather_fragment_container, currentWeatherFragment)
+                    .add(R.id.forecast_weather_fragment_container, forecastWeatherFragment)
+                    .commit();
+        } else {
+            currentWeatherFragment = (CurrentWeatherFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.current_weather_fragment_container);
+            forecastWeatherFragment = (ForecastWeatherFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.forecast_weather_fragment_container);
+        }
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (!hasRequiredPermissions()){
-            maybeShowRequestPermissions();
+        googleApiClient.connect();
+        if (needsDeniedPermission || !isNetworkConnection()) {
+            showNeedsPermissionDialog();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        googleApiClient.disconnect();
     }
 
     @Override
@@ -49,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -57,47 +91,66 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Checks to see if there is a network connection.
+     * @return true if there is a network connection and is connected.
+     */
+    private boolean isNetworkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    //
+    // GOOGLE PLAY SERVICES
+    //
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+            requestRequiredPermissions();
+            return;
+        }
+        // update fragments with last location.
+        Location lastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(googleApiClient);
+        if (lastLocation != null) {
+            currentWeatherFragment.updateLocation(lastLocation);
+            forecastWeatherFragment.updateLocation(lastLocation);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        showNeedsPermissionDialog();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        showNeedsPermissionDialog();
+    }
+
+    // PERMISSIONS
+
     private void requestRequiredPermissions() {
         // need internet and location permission
         String[] perms = new String[2];
         perms[0] = "android.permission.ACCESS_FINE_LOCATION";
-        perms[1] = "android.permission.INTERNET";
+        perms[1] = "android.permission.ACCESS_COARSE_LOCATION";
         ActivityCompat.requestPermissions(this, perms, MY_PERMISSIONS_REQUEST);
-    }
-
-    private boolean maybeShowRequestPermissions() {
-        if (needsDeniedPermission ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.INTERNET) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
-            showNeedsPermissionDialog();
-            return true;
-        }
-        return false;
-    }
-
-    public boolean hasRequiredPermissions() {
-        final int curApiVersion = Build.VERSION.SDK_INT;
-        if (curApiVersion < Build.VERSION_CODES.M){
-            return true;
-        }
-        if (ContextCompat.checkSelfPermission(
-                this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                &&
-            ContextCompat.checkSelfPermission(
-                this,Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED ){
-            return true;
-        }
-        return false;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         boolean hasPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                                grantResults[1] == PackageManager.PERMISSION_GRANTED;
-        if (!hasPermission){
+                grantResults[1] == PackageManager.PERMISSION_GRANTED;
+        if (!hasPermission) {
             needsDeniedPermission = true;
         }
     }
@@ -106,4 +159,5 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         RequiresPermissionsDialogFragment dialog = new RequiresPermissionsDialogFragment();
         dialog.show(getSupportFragmentManager(), "Permissions");
     }
+
 }
