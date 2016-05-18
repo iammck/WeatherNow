701181bf121c5aaba251d4a86d3b6f02f2db4020
Michael King <michaeldigit@gmail.com>
2016-05-18 09:16:19 -0700

    include temp openweathermap id
    include an id the project can be built with that will work with the OpenWeatherMap API. This key is temporary.

--- app/src/main/java/com/mck/weathernow/Constants.java
+++ app/src/main/java/com/mck/weathernow/Constants.java
@@ -4,5 +4,7 @@ package com.mck.weathernow;
  * Created by Michael on 5/15/2016.
  */
 public class Constants {
-    public static final String API_ID = PrivateConstants.apiKey;
+    private static String tempKey = "cfc3a8fdc26a4aee11330801960e85fa";
+    //public static final String API_ID = PrivateConstants.apiKey;
+    public static final String API_ID = tempKey;
 }
