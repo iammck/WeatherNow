3626a07bf214742094a3ac6e0f05b7bc2ac8698d
Michael King <michaeldigit@gmail.com>
2016-05-17 22:56:01 -0700

    accept and return a request id
    accepts and returns a request id with the result.

--- app/src/main/java/com/mck/weathernow/asynctask/GetWeatherIconAsyncTask.java
+++ app/src/main/java/com/mck/weathernow/asynctask/GetWeatherIconAsyncTask.java
@@ -6,13 +6,15 @@ import android.os.AsyncTask;
 import com.mck.weathernow.service.OpenWeatherMapService;
 
 /**
- * AsyncTask to get the current weather. requires the call, lat and long
+ * AsyncTask to get a weather icon requires the callback, icon id, and requestId.
  * to start.
  * Created by Michael on 5/16/2016.
  */
 public class GetWeatherIconAsyncTask extends AsyncTask<Object,Integer,Bitmap>{
+    private Integer reqId;
+
     public interface callback {
-        void onWeatherIconResult(Bitmap icon);
+        void onWeatherIconResult(Bitmap icon, Integer requestId);
     }
 
     GetWeatherIconAsyncTask.callback callback;
@@ -20,15 +22,18 @@ public class GetWeatherIconAsyncTask extends AsyncTask<Object,Integer,Bitmap>{
     @Override
     protected Bitmap doInBackground(Object... params) {
         callback = (GetWeatherIconAsyncTask.callback) params[0];
-        String id = (String) params[1];
-        return OpenWeatherMapService.instance().requestWeatherIcon(id);
+        String iconId = (String) params[1];
+        if (params.length > 2){
+            reqId = (Integer) params[2];
+        }
+        return OpenWeatherMapService.instance().requestWeatherIcon(iconId);
     }
 
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
         if (!isCancelled()){
-            callback.onWeatherIconResult(bitmap);
+            callback.onWeatherIconResult(bitmap, reqId);
         }
     }
 }
