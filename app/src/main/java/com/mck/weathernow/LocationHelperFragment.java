9c714f18eca5ebc6b3011d9ae06839caf456f480
Michael King <michaeldigit@gmail.com>
2016-05-29 12:57:58 -0700

    include location update helepr
    removed request location code from main activity and put into a fragment.  Fragment is added in on start. Once a location is received having great accuracy, the helper fragment is removed from frag manager.

---
+++ app/src/main/java/com/mck/weathernow/LocationHelperFragment.java
@@ -0,0 +1,148 @@
+package com.mck.weathernow;
+
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+/**
+ *
+ * Created by Michael on 5/28/2016.
+ */
+public class LocationHelperFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, LocationListener, GoogleApiClient.OnConnectionFailedListener {
+    public static final String TAG = "LocHelper";
+
+    private static final int MY_PERMISSIONS_ACCESS_LOCATION = 23;
+    private static final int REQUEST_CHECK_SETTINGS = 72;
+    private static final int CONN_FAIL_RESLN_RSLT = 43;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest locationRequest;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // create the googleApiClient for access to google services.
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mGoogleApiClient != null && mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // create location request instance
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(1000);
+        //locationRequest.setSmallestDisplacement(100);// in meters.
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        // Make sure location services are enabled.
+        // get a location settings builder and check location settings via a PendingResult
+        LocationSettingsRequest.Builder settingsBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> pendingResult =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, settingsBuilder.build());
+        pendingResult.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+                Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can
+                        // Make sure the app has location permissions. If it does not,
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                                PackageManager.PERMISSION_GRANTED &&
+                                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                                        PackageManager.PERMISSION_GRANTED) {
+                            // Should we show an explanation?
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                                // Show an explanation to the user *asynchronously* -- don't block
+                                // this thread waiting for the user's response! After the user
+                                // sees the explanation, try again to request the permission.
+                                ((MainActivity) getActivity()).onPermissionsDenied();
+
+                            } else {
+                                // No explanation needed, we can request the permission.
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                                Manifest.permission.ACCESS_COARSE_LOCATION },
+                                        MY_PERMISSIONS_ACCESS_LOCATION);
+                            }
+                            return;
+                        }
+                        // have permissions, go ahead and request updates.
+                        LocationServices.FusedLocationApi.requestLocationUpdates(
+                                mGoogleApiClient, locationRequest, LocationHelperFragment.this);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    getActivity(),
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                            e.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        ((MainActivity) getActivity()).onLocationsSettingsFailure();
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        ((MainActivity) getActivity()).onLocationUpdate(location);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        try {
+            connectionResult.startResolutionForResult(getActivity(), CONN_FAIL_RESLN_RSLT);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/mck/weathernow/MainActivity.java
+++ app/src/main/java/com/mck/weathernow/MainActivity.java
@@ -1,42 +1,16 @@
 package com.mck.weathernow;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
 
 public class MainActivity extends AppCompatActivity
-        implements ActivityCompat.OnRequestPermissionsResultCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private static final int MY_PERMISSIONS_REQUEST = 72; // for when making permissions
-    private boolean needsDeniedPermission = false; // if was denied a necessary permission.
-    private CurrentWeatherFragment currentWeatherFragment;
-    private ForecastWeatherFragment forecastWeatherFragment;
-    private GoogleApiClient googleApiClient;
-    private Location lastLocation;
+    // looking for location with great accuracy,
+    private static final float GREAT_ACCURACY = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,163 +18,43 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         // if there is no saved instance state
         if (savedInstanceState == null) {
-            currentWeatherFragment = new CurrentWeatherFragment();
-            forecastWeatherFragment = new ForecastWeatherFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.current_weather_fragment_container, currentWeatherFragment)
-                    .add(R.id.forecast_weather_fragment_container, forecastWeatherFragment)
+            // set up the weather fragment
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocationHelperFragment locFrag = (LocationHelperFragment)
+                getSupportFragmentManager().findFragmentByTag(LocationHelperFragment.TAG);
+        if (locFrag == null) {
+            locFrag = new LocationHelperFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(locFrag, LocationHelperFragment.TAG)
                     .commit();
-        } else {
-            currentWeatherFragment = (CurrentWeatherFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.current_weather_fragment_container);
-            forecastWeatherFragment = (ForecastWeatherFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.forecast_weather_fragment_container);
-        }
-        if (googleApiClient == null) {
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        googleApiClient.connect();
-        if (needsDeniedPermission || !isNetworkConnection()) {
-            showNeedsPermissionDialog();
+    public void onLocationUpdate(Location location) {
+        Log.v("MainActivity", "onLocationUpdate " + location.toString());
+        // if the accuracy is great, remove location helper fragment.
+        if (location.getAccuracy() < GREAT_ACCURACY){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(getSupportFragmentManager().findFragmentByTag(LocationHelperFragment.TAG))
+                    .commit();
         }
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        googleApiClient.disconnect();
-    }
-
-    /*// no menu yet
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    /**
-     * Checks to see if there is a network connection.
-     * @return true if there is a network connection and is connected.
-     */
-    private boolean isNetworkConnection() {
-        ConnectivityManager connectivityManager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
-    }
-
-    //
-    // GOOGLE PLAY SERVICES
-    //
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // check for permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-            requestRequiredPermissions();
-            return;
-        }
-        // update fragments with last location.
-        lastLocation = LocationServices.FusedLocationApi
-                .getLastLocation(googleApiClient);
-        if (lastLocation != null) {
-            currentWeatherFragment.updateLocation(lastLocation);
-            forecastWeatherFragment.updateLocation(lastLocation);
-        }
-
-        // create location request instance
-        final LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(20000);
-        locationRequest.setFastestInterval(10000);
-        locationRequest.setSmallestDisplacement(100);// in meters.
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-
-        // get a location settings builder
-        LocationSettingsRequest.Builder settingsBuilder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        // get the settings and if not able to use location show dialog.
-        PendingResult<LocationSettingsResult> pendingResult =
-                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, settingsBuilder.build());
-        pendingResult.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
-                Status status = locationSettingsResult.getStatus();
-                if (!status.isSuccess()) {
-                    showNeedsPermissionDialog();
-                }
-            }
-        });
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                googleApiClient, locationRequest, MainActivity.this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        showNeedsPermissionDialog();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        showNeedsPermissionDialog();
-    }
-
-    // PERMISSIONS
-
-    private void requestRequiredPermissions() {
-        // need internet and location permission
-        String[] perms = new String[2];
-        perms[0] = "android.permission.ACCESS_FINE_LOCATION";
-        perms[1] = "android.permission.ACCESS_COARSE_LOCATION";
-        ActivityCompat.requestPermissions(this, perms, MY_PERMISSIONS_REQUEST);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        boolean hasPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                grantResults[1] == PackageManager.PERMISSION_GRANTED;
-        if (!hasPermission) {
-            needsDeniedPermission = true;
-        }
-    }
-
-    private void showNeedsPermissionDialog() {
+    public void onPermissionsDenied() {
         RequiresPermissionsDialogFragment dialog = new RequiresPermissionsDialogFragment();
         dialog.show(getSupportFragmentManager(), "Permissions");
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        lastLocation = location;
-        if (lastLocation != null) {
-            currentWeatherFragment.updateLocation(lastLocation);
-            forecastWeatherFragment.updateLocation(lastLocation);
-        }
+    public void onLocationsSettingsFailure() {
+        LocationSettingsFailureDialogFragment dialog = new LocationSettingsFailureDialogFragment();
+        dialog.show(getSupportFragmentManager(), "Permissions");
     }
+
 }
