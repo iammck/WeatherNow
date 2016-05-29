4038acafe857f682a191af6bee5f0c47df054c30
Michael King <michaeldigit@gmail.com>
2016-05-29 12:53:11 -0700

    Update fail dialogs
    Include a new dialog for location settings failure.

--- app/src/main/java/com/mck/weathernow/LocationSettingsFailureDialogFragment.java
+++ app/src/main/java/com/mck/weathernow/LocationSettingsFailureDialogFragment.java
@@ -8,21 +8,20 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
 
 /**
- * Shows a dialog box that informs the user that this app requires perms to work.
- * Created by Michael on 5/16/2016.
+ * LocationSettingsFailureDialogFragment
+ * Created by Michael on 5/29/2016.
  */
-public class RequiresPermissionsDialogFragment extends DialogFragment {
+public class LocationSettingsFailureDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use a builder to construct the dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View layout = inflater.inflate(R.layout.requires_perm_fragment, null);
-        builder .setView(layout)
+        View layout = inflater.inflate(R.layout.loc_settings_fail_fragment, null);
+        builder.setView(layout)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -31,6 +30,5 @@ public class RequiresPermissionsDialogFragment extends DialogFragment {
         AlertDialog result = builder.create();
         return result;
     }
-
 
 }
